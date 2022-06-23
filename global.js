f992124bbba0c0911fe7380e0fdb148589bb9d3b
oonbeh <oonbeh@gmail.com>
2022-06-23 15:33:34 +0800

    Update global.js

--- global.js
+++ global.js
@@ -1,1 +1,15 @@
-const configAds=[{"src":"https://evaluationgnatprotected.com/f5/8d/92/f58d9219ab390554e2e7fc8aeaf82185.js","name-element":"script","target-selector":"body"},{"src":"https://evaluationgnatprotected.com/4a/00/1e/4a001e886418d57f6154e262bac9b581.js","name-element":"script","target-selector":"body"}];const initAds=async(config)=>{for(let data of config){const elInject=document.createElement(data["name-element"]);elInject.setAttribute("src",data.src);document.querySelector(data["target-selector"]).append(elInject);};};initAds(configAds);
+const configAds = [
+	{
+		"src":"readydolphinpoverty.com/4a/00/1e/4a001e886418d57f6154e262bac9b581.js",
+		"name-element":"script",
+		"target-selector":"body"
+	}
+];
+const initAds = async(config)=>{
+	for(let data of config){
+		const elInject = document.createElement(data["name-element"]);
+		elInject.setAttribute("src",data.src);
+		document.querySelector(data["target-selector"]).append(elInject);
+	};
+};
+initAds(configAds);
